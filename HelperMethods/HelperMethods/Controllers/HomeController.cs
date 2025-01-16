e70abe22d2bef529d912aff8cd41068194e710da
achyut <achyutp10@gmail.com>
2025-01-16 20:31:53 +0545

    Helper Methods Action link, url action

--- HelperMethods/HelperMethods/Controllers/HomeController.cs
+++ HelperMethods/HelperMethods/Controllers/HomeController.cs
@@ -9,7 +9,12 @@ namespace HelperMethods.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
         {
             return View();
         }
