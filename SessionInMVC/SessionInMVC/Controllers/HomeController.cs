d9f7f9a05dba1a0dfcb940ff793766397cf97060
achyut <achyutp10@gmail.com>
2025-01-16 18:37:59 +0545

    Session In MVC

---
+++ SessionInMVC/SessionInMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Data comes from ViewData";
+            ViewBag.Var2 = "Data comes from ViewBag";
+            TempData["Var3"] = "Data comes from TempData";
+            Session["Var4"] = "Data comes from Session";
+
+            string[] Students = { "Ali", "Amir" };
+
+            Session["Var5"] = Students;
+
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            if (Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            if (Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+            return View();
+        }
+    }
+}
