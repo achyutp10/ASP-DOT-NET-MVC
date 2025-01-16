fa9497f1b6677569bb193a4f898f93124d8beb38
achyut <achyutp10@gmail.com>
2025-01-16 14:28:37 +0545

    TempData keep()

---
+++ TempDataDemo/TempDataDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempDataDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Message from View Data";
+            ViewBag.Var2 = "Message from View Bag";
+            TempData["Var3"] = "Message from Temp Data";
+
+            //string[] games = { "Cri", "Foot", "Base" };
+            //TempData["GamesArr"] = games;
+            return RedirectToAction("About");
+            //return View();
+        }
+        public ActionResult About()
+        {
+            //TempData.Keep("GamesArr");
+            if (TempData["Var3"] != null)
+            {
+                TempData["Var3"].ToString();
+            }
+            TempData.Keep("Var3");
+            return View();
+        }
+
+        public ActionResult Contact() {
+
+            if (TempData["Var3"] != null)
+            {
+                TempData["Var3"].ToString();
+            }
+            TempData.Keep("Var3");
+            return View();
+        }
+    }
+}
