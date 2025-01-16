087a7a367c1de43954e463f6d7e99936af5d5064
achyut <achyutp10@gmail.com>
2025-01-16 12:08:17 +0545

    ViewBag ViewData Differences COm

---
+++ Difference_ViewBag_ViewData/Difference_ViewBag_ViewData/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Difference_ViewBag_ViewData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Difference_ViewBag_ViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Message1"] = "Data comes from ViewData";
+            ViewBag.Message2 = "Data comes from ViewBag";
+
+            ViewData["CurrDate1"] = DateTime.Now.ToString();
+            ViewBag.CurrDate2 = DateTime.Now.ToString();
+
+            string[] games = { "Hockey", "Football" };
+            ViewData["GamesArray1"] = games;
+            ViewBag.GamesArray2 = games;
+
+            Employee achyut = new Employee();
+
+            achyut.Id = 1;
+            achyut.Name = "Achyut";
+            achyut.Desc = "Juior";
+
+            ViewData["Em1"] = achyut;
+            ViewBag.Em2 = achyut;
+
+            return View();
+        }
+    }
+}
