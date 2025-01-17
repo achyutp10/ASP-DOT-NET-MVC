c0865a19ec04de768b4e8b3bb8fc364eee65aa7b
achyut <achyutp10@gmail.com>
2025-01-17 18:04:37 +0545

    Data Annotation

--- DataAnnotations/DataAnnotations/Controllers/HomeController.cs
+++ DataAnnotations/DataAnnotations/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DataAnnotations.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,51 @@ namespace DataAnnotations.Controllers
 {
     public class HomeController : Controller
     {
+        string EmailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$";
+
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee e)
+        {
+
+            if (e.Name.Equals("") == true)
+            {
+                ModelState.AddModelError("Name", "Name is Required");
+                ViewData["fullNameError"] = "*";
+            }
+            if (e.Age.Equals("") == true)
+            {
+                ModelState.AddModelError("Age", "Age is Required");
+                ViewData["AgeError"] = "*";
+            }
+            if (e.Gender.Equals("") == true)
+            {
+                ModelState.AddModelError("Age", "Age is Required");
+                ViewData["AgeError"] = "*";
+            }
+            if (e.Email.Equals("") == true)
+            {
+                ModelState.AddModelError("Email", "Email is Required");
+                ViewData["EmailError"] = "*";
+            }
+            else
+            {
+                if (Regex.IsMatch(e.Email, EmailPattern) == false)
+                {
+                    ModelState.AddModelError("Email", "Invalid Email");
+                    ViewData["EmailError"] = "*";
+                }
+            }
+
+            if (ModelState.IsValid == true)
+            {
+                ViewData["SuccessMessage"] = "<script>alert('Data has been submitted')</script>";
+                ModelState.Clear();
+            }
             return View();
         }
     }
---
+++ DataAnnotations/DataAnnotations/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataAnnotations.Models
+{
+    public class Employee
+    {
+        [Required (ErrorMessage = "Id is Mandatory")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is Mandatory")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Age is Mandatory")]
+        public int? Age { get; set; }        
+        [Required(ErrorMessage = "Gender is Mandatory")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Email is Mandatory")]
+        public string Email { get; set; }
+    }
+}
