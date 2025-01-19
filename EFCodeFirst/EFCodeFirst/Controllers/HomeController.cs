fcccd0591ff312ca8c93b86dd29a1b9f837b0357
Achyut <achyutp10@gmail.com>
2025-01-19 13:59:10 +0545

    Code First Entity Framework

---
+++ EFCodeFirst/EFCodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirst.Models;
+
+namespace EFCodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentContext db = new StudentContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList(); 
+            return View(data);
+        }
+    }
+}
