7d55bd6a8bfb6363d03264a9534992db601f0f91
Achyut <achyutp10@gmail.com>
2025-01-21 08:48:58 +0545

    Cookies Completed

--- CookiesDemo/CookiesDemo/Controllers/HomeController.cs
+++ CookiesDemo/CookiesDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CookiesDemo.Models;
 
 namespace CookiesDemo.Controllers
 {
@@ -11,6 +12,21 @@ namespace CookiesDemo.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                HttpCookie cookie = new HttpCookie("Username");
+                cookie.Value = u.Username;
+
+                HttpContext.Response.Cookies.Add(cookie);
+                cookie.Expires = DateTime.Now.AddDays(2);
+                return RedirectToAction("Index", "Dashboard");
+            }
             return View();
         }
     }
