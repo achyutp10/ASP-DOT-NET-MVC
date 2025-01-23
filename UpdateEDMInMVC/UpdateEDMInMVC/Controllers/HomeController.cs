102b6b5d6c87837f1dacc26fdb2f258a42fb22af
Achyut <achyutp10@gmail.com>
2025-01-23 12:32:55 +0545

    Update entity data model using EF

---
+++ UpdateEDMInMVC/UpdateEDMInMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UpdateEDMInMVC.Models;
+
+namespace UpdateEDMInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        EDMExampleMVCEntities db = new EDMExampleMVCEntities();
+
+        public ActionResult Index()
+        {
+            var data = db.students.ToList();
+            return View(data);
+        }
+        public ActionResult Employee()
+        {
+            var data = db.empolyees.ToList();
+            return View(data);
+        }
+    }
+}
