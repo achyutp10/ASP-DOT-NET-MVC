6e6c2a369be24b7b1e1b5076b8affc2dafaee794
Achyut <achyutp10@gmail.com>
2025-01-23 17:16:21 +0545

    Authorize filter MVC

---
+++ AuthorizeFilter/AuthorizeFilter/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AuthorizeFilter.Models;
+
+namespace AuthorizeFilter.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(user u, string ReturnUrl)
+        {
+            return View();
+        }
+    }
+}
