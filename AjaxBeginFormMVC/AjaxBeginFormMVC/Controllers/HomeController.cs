2e83a9ac68ef1d844d42500cecec48a3fd49c784
Achyut <achyutp10@gmail.com>
2025-01-24 10:11:58 +0545

    Ajax begin form

---
+++ AjaxBeginFormMVC/AjaxBeginFormMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxBeginFormMVC.Models;
+
+namespace AjaxBeginFormMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        EmployeeDBEntities1 db = new EmployeeDBEntities1();
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(employee e)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.employees.Add(e);
+                int a = db.SaveChanges();
+                if (a > 0) 
+                {
+                    return Json("Data Inserted !!"); 
+                }
+                else
+                {
+                    return Json("Data Not Inserted !!");
+                }
+            }
+            return View();
+        }
+
+
+    }
+}
