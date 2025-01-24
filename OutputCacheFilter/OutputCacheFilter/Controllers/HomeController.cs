baa081902bb910580dd267d5dc6c05ab2e5e01af
Achyut <achyutp10@gmail.com>
2025-01-24 08:43:10 +0545

    Output cache filter

---
+++ OutputCacheFilter/OutputCacheFilter/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OutputCacheFilter.Models;
+
+namespace OutputCacheFilter.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        PersonEntities db = new PersonEntities();
+        // GET: Home
+        //[OutputCache(Duration = 10, Location = System.Web.UI.OutputCacheLocation.Server)]
+        //[OutputCache(Duration = 10, Location = System.Web.UI.OutputCacheLocation.Any)]
+        [OutputCache(Duration = 10)]
+        public ActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        [OutputCache(Duration = 40)]
+        public ActionResult GetData()
+        {
+
+            var data = db.people.ToList();
+            return View(data);
+        }
+    }
+}
