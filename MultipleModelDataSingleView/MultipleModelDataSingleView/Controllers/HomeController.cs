fc203069106ec49fc7765cf5b037fc9bb2e85f3f
Achyut <achyutp10@gmail.com>
2025-01-25 11:18:45 +0545

    Multiple model data in a single view

---
+++ MultipleModelDataSingleView/MultipleModelDataSingleView/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultipleModelDataSingleView.Models;
+
+namespace MultipleModelDataSingleView.Controllers
+{
+    public class HomeController : Controller
+    {
+        SchoolDBEntities db = new SchoolDBEntities();
+
+        public ActionResult Index()
+        {
+            //var std = getStudents();
+            //var tchr = getTeachers();
+
+            //MultiModelData data = new MultiModelData();
+            //data.my_students = std;
+            //data.my_teachers = tchr;
+
+            //ViewBag.students = std;    
+            //ViewData["students"] = std;    
+            //ViewBag.teachers = tchr;   
+            
+            return View();
+        }
+
+        public PartialViewResult Students()
+        {
+            var std = getStudents();
+            return PartialView("_students",std);
+        }
+        public PartialViewResult Teachers()
+        {
+            var tchr = getTeachers();
+            return PartialView("_teachers",tchr);
+        }
+        public List<student> getStudents()
+        {
+            return db.students.ToList();
+        }
+        public List<teacher> getTeachers()
+        {
+            return db.teachers.ToList();
+        }
+    }
+}
---
+++ MultipleModelDataSingleView/MultipleModelDataSingleView/Models/MultiModelData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultipleModelDataSingleView.Models
+{
+    public class MultiModelData
+    {
+        public List<student> my_students { get; set; }
+        public List<teacher> my_teachers { get; set; }
+    }
+}
